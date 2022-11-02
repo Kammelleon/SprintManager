bdabef6fdf65bbaa924fe7ed56df22af1cbb2dc8
Kammelleon <kamilbugla22@icloud.com>
2022-11-02 17:57:06 +0100

    Created models for Tasks

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskStatus(models.Model):
+    ready_to_take = models.BooleanField(default=False)
+    in_progress = models.BooleanField(default=False)
+    ready_to_verify = models.BooleanField(default=False)
+    done = models.BooleanField(default=False)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.OneToOneField(TaskStatus, on_delete=models.CASCADE)
+    planned_ords = models.FloatField()
+    storypoints = models.FloatField()
+    real_ords = models.FloatField()
